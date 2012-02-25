90fbb460e1575f9c4cc9303f00f6e7cf303e9111
Gianmaria Dal Maistro <gianmaria.dalmaistro@me.com>
2012-02-25 19:55:21 +0100

    initial project commit

---
+++ GPS Reminder/GPS Reminder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GPS Reminder
+//
+//  Created by Jerry on 25/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
